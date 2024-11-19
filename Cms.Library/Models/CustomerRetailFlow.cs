23602189ca520f1a443b63e9d5a69c8f117e6045
asbszt <asbszt@live.cn>
2024-11-19 22:05:19 +0800

    2024/11/19

--- Cms.Library/Models/CustomerRetailFlow.cs
+++ Cms.Library/Models/CustomerRetailFlow.cs
@@ -1,12 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Cms.Library.Models;
 
 public class CustomerRetailFlow
 {
     public Guid Id { get; set; }
+    [MaxLength (200)]
     public string RetailId { get; set; }
     public int SerialNumber { get; set; }
+    [MaxLength (50)]
     public string ItemId { get; set; }
+    [MaxLength(300)]
     public string ItemName { get; set; }
+    [MaxLength(60)]
     public string ItemBarcode { get; set; }
     public decimal ItemPrice { get; set; }
     public decimal ItemQuantity { get; set; }
--- Cms.Library/Models/CustomerRetailMaster.cs
+++ Cms.Library/Models/CustomerRetailMaster.cs
@@ -1,14 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Cms.Library.Models;
 
 public class CustomerRetailMaster
 {
     public Guid Id { get; set; }
+    [MaxLength(200)]
     public string RetailId { get; set; }
     public decimal RetailAmount{ get; set; }
+    [MaxLength (50)]
     public string CustomerId { get; set; }
     public DateTime RetailDate { get; set; }
-    public string InvoiceStatus { get; set; }
+    
+    public int InvoiceStatus { get; set; }
+    [MaxLength(200)]
     public string InvoiceNumber { get; set; }
+    [MaxLength(1000)]
     public string RetailMemo { get; set; }
     public List<CustomerRetailFlow> CustomerRetailFlows { get; set; }
 }
