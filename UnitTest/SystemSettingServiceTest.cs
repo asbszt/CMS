51a10e326f018c410b3b9b98bc55b1da7db6ae4e
asbszt <asbszt@live.cn>
2024-11-22 18:31:44 +0800

    2024/11/22

--- UnitTest/SystemSettingServiceTest.cs
+++ UnitTest/SystemSettingServiceTest.cs
@@ -5,9 +5,13 @@ namespace UnitTest;
 
 public class SystemSettingServiceTest : IDisposable
 {
+    //通过构造函数清理资源
+    
+
+    //通过垃圾回收调用释放来清理资源
     public void Dispose()
     {
-        throw new NotImplementedException();
+
     }
 
     [Fact]
