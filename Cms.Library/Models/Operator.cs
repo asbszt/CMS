9c92beebc93dc0fb7fed14de9ba427b16415c1a0
asbszt <asbszt@live.cn>
2024-11-24 23:17:53 +0800

    2024/11/24

---
+++ Cms.Library/Models/Operator.cs
@@ -0,0 +1,10 @@
+namespace Cms.Library.Models;
+
+public class Operator
+{
+    public Guid Id { get; set; }
+    public string OperId { get; set; }
+    public string OperName { get; set; }
+    public string OperGroup { get; set; }
+    public string OperDescription { get; set; }
+}
